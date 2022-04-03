3eeb1043347267c268b169157fa8fad8335b70ca
MarlenedosSantos <86972378+MarlenedosSantos@users.noreply.github.com>
2022-04-03 15:59:30 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
+import Botones from './components/Botones/Botones'
+import Historial from './components/Historial/Historial';
+import Historia from './components/Historia/Historia';
+import Disenio from './components/Disenio/Disenio'
+
 function App() {
   return (
-    <div className="App">
+    <div className="layout">
+      <Disenio/>
     </div>
   );
 }
